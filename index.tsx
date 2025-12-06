3e4b4319b664878fb7284441f3c1935250c58dbc
zway <zwaydot@gmail.com>
2025-12-06 22:18:20 +0800

    feat: optimize exchange snapshot component - text color, shimmer effect and date display

--- index.tsx
+++ index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "@fontsource/geist/400.css";
+import "@fontsource/geist/500.css";
+import "@fontsource/geist/600.css";
+import "@fontsource/geist-mono/500.css";
 import App from './App';
 
 const rootElement = document.getElementById('root');
